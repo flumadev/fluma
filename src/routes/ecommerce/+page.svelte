d3b44aa28957e0829e0877857cd97c1bc85f2bc1
Luis Amorim <luisfelipeamorim@hotmail.com>
2023-04-13 00:03:37 -0300

    feat: ecommerce

---
+++ src/routes/ecommerce/+page.svelte
@@ -0,0 +1,119 @@
+<main>
+    <header class="section relative lg:pb-48">
+        <div class="container"><h1 class="title text-lg sm:text-2xl md:text-4xl md:font-light max-w-screen-md mb-20">
+            Ecommerce</h1>
+            <div class="flex max-md:flex-col relative">
+                <div class="space-y-7 md:w-1/3 relative z-20 md:mr-16 max-md:mb-8">
+                    <div class="title md:text-xl"><h2>Headless Commerce</h2></div>
+                    <p class="">O comércio eletrônico teve um aumento incrível nos últimos anos. Agora, empresas de
+                        todos os tamanhos e setores podem criar suas próprias lojas virtuais para alcançar
+                        novos clientes. Na Fluma, nos orgulhamos de criar lojas que fornecem a
+                        melhor experiência de compra online por meio de uma combinação de tecnologia de ponta e
+                        personalização.</p>
+                </div>
+                <img class="md:w-2/3 relative z-20"
+                     src="https://images.prismic.io/monogram/dd53b136-f625-400c-98ff-a074363ccd49_ecommerce.png?auto=compress,format"
+                     width="515" height="298" sizes="(max-width: 640px) 100vw, 50vw"
+                     srcset="https://images.prismic.io/monogram/dd53b136-f625-400c-98ff-a074363ccd49_ecommerce.png?auto=compress%2Cformat&amp;quality=75&amp;width=512 512w,https://images.prismic.io/monogram/dd53b136-f625-400c-98ff-a074363ccd49_ecommerce.png?auto=compress%2Cformat&amp;quality=75&amp;width=768 768w,https://images.prismic.io/monogram/dd53b136-f625-400c-98ff-a074363ccd49_ecommerce.png?auto=compress%2Cformat&amp;quality=75&amp;width=1024 1024w,https://images.prismic.io/monogram/dd53b136-f625-400c-98ff-a074363ccd49_ecommerce.png?auto=compress%2Cformat&amp;quality=75&amp;width=1920 1920w,https://images.prismic.io/monogram/dd53b136-f625-400c-98ff-a074363ccd49_ecommerce.png?auto=compress%2Cformat&amp;quality=75&amp;width=2560 2560w"
+                     draggable="false" loading="eager"></div>
+        </div>
+    </header>
+    <section class="list-grid section relative overflow-hidden ">
+        <div class="glow-brass blur-large glow-motion top-56 -right-20 w-96 h-96 opacity-20 -z-10"
+             aria-hidden="true"></div>
+        <div class="glow-brass blur-large glow-motion top-96 left-36 w-[24rem] h-[24rem] -z-10 opacity-20"
+             aria-hidden="true" style="animation-delay: 2s;"></div>
+        <div class="glow-brass blur-large glow-motion -top-20 -left-44 w-[34rem] h-[34rem] -z-10 opacity-30"
+             aria-hidden="true" style="animation-delay: 4s;"></div>
+        <div class="container max-w-screen-lg">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-x-36 lg:gap-y-28">
+                <div class="">
+                    <svg class="fill-current h-12 w-12 mb-8 text-gray-400" viewBox="0 0 512 512" focusable="false"
+                         role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM256 496C282 496 309.9 474.5 332.2 429.9C343.2 407.9 352.1 381.5 358.4 352H153.6C159.9 381.5 168.8 407.9 179.8 429.9C202.1 474.5 229.1 496 255.1 496H256zM150.6 336H361.4C365.7 311 368 284.1 368 256C368 227.9 365.7 200.1 361.4 176H150.6C146.3 200.1 144 227.9 144 256C144 284.1 146.3 311 150.6 336V336zM358.4 160C352.1 130.4 343.2 104.1 332.2 82.14C309.9 37.51 282 16 256 16C229.1 16 202.1 37.51 179.8 82.14C168.8 104.1 159.9 130.4 153.6 160H358.4zM377.6 176C381.8 201.2 384 228.1 384 256C384 283.9 381.8 310.8 377.6 336H482.3C491.2 310.1 496 284.1 496 256C496 227.9 491.2 201 482.3 176H377.6zM307.5 21.53C337.9 48.31 362.2 98.15 374.7 160H476C445.6 90.45 383.2 38.09 307.5 21.53V21.53zM204.5 21.53C128.8 38.09 66.36 90.45 35.97 160H137.3C149.8 98.15 174.1 48.31 204.5 21.53V21.53zM16 256C16 284.1 20.81 310.1 29.66 336H134.4C130.2 310.8 128 283.9 128 256C128 228.1 130.2 201.2 134.4 176H29.66C20.81 201 16 227.9 16 256zM476 352H374.7C362.2 413.8 337.9 463.7 307.5 490.5C383.2 473.9 445.6 421.5 476 352V352zM137.3 352H35.97C66.36 421.5 128.8 473.9 204.5 490.5C174.1 463.7 149.8 413.8 137.3 352z"></path>
+                    </svg>
+                    <h3 class="title mb-4">Arquitetura Headless Commerce</h3>
+                    <p style="max-width: 340px" class="relative z-10 text-gray-400/60">Headless Commerce architecture é
+                        a dissociação da camada de apresentação de front-end de um site da funcionalidade de e-commerce
+                        de back-end.</p></div>
+                <div class="">
+                    <svg class="fill-current h-12 w-12 mb-8 text-gray-400" viewBox="0 0 576 512" focusable="false"
+                         role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M112 152.5V104c0-30.88 25.12-56 56-56h48C220.4 48 224 44.42 224 40S220.4 32 216 32h-48C128.3 32 96 64.3 96 104v48.45c0 41.08-22.83 78.02-59.58 96.39C33.72 250.2 32 252.1 32 256s1.719 5.797 4.422 7.156C73.17 281.5 96 318.5 96 359.5V408C96 447.7 128.3 480 168 480h48C220.4 480 224 476.4 224 472S220.4 464 216 464h-48c-30.88 0-56-25.12-56-56v-48.45C112 317.2 90.89 278.7 56.06 256C90.89 233.3 112 194.8 112 152.5zM539.6 248.8C502.8 230.5 480 193.5 480 152.5V104C480 64.3 447.7 32 408 32h-48C355.6 32 352 35.58 352 40s3.578 8 8 8h48c30.88 0 56 25.12 56 56v48.45C464 194.8 485.1 233.3 519.9 256C485.1 278.7 464 317.2 464 359.5V408c0 30.88-25.12 56-56 56h-48c-4.422 0-8 3.578-8 8S355.6 480 360 480h48c39.7 0 72-32.3 72-72v-48.45c0-41.08 22.83-78.02 59.58-96.39C542.3 261.8 544 259 544 256S542.3 250.2 539.6 248.8z"></path>
+                    </svg>
+                    <h3 class="title mb-4">Construído Desacoplado</h3>
+                    <p style="max-width: 340px" class="relative z-10 text-gray-400/60">Os desenvolvedores podem utilizar
+                        sua tecnologia de front-end preferida para fornecer experiências de conteúdo de alta qualidade e
+                        conectar uma solução de comércio eletrônico no back-end que gerencia toda a funcionalidade de
+                        comércio.</p></div>
+            </div>
+        </div>
+    </section>
+    <section class="list-grid section relative overflow-hidden ">
+        <div class="glow-brass blur-large glow-motion top-56 -right-20 w-96 h-96 opacity-20 -z-10"
+             aria-hidden="true"></div>
+        <div class="glow-brass blur-large glow-motion top-96 left-36 w-[24rem] h-[24rem] -z-10 opacity-20"
+             aria-hidden="true" style="animation-delay: 2s;"></div>
+
+        <div class="container max-w-screen-lg"><h2
+                class="title text-lg sm:text-2xl text-center mb-12 md:mb-20 lg:mb-36">Headless ecommerce</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-x-36 lg:gap-y-28">
+                <div class="">
+                    <svg class="fill-current h-12 w-12 mb-8 text-gray-400" viewBox="0 0 512 512" focusable="false"
+                         role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 96C0 60.65 28.65 32 64 32H448C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96zM128 144H496V96C496 69.49 474.5 48 448 48H128V144zM112 48H64C37.49 48 16 69.49 16 96V144H112V48zM16 160V416C16 442.5 37.49 464 64 464H448C474.5 464 496 442.5 496 416V160H16z"></path>
+                    </svg>
+                    <h3 class="title mb-4">Sites rápidos</h3>
+                    <p style="max-width: 340px" class="relative z-10 text-gray-400/60">Aproveite tecnologias modernas e
+                        avançadas para criar sites visionários e rápidos por meio de pré-renderização de página, o que
+                        resulta em melhor desempenho e melhor SEO. As páginas pré-renderizadas tornam a página
+                        totalmente interativa por meio da hydration.</p></div>
+                <div class="">
+                    <svg class="fill-current h-12 w-12 mb-8 text-gray-400" viewBox="0 0 640 512" focusable="false"
+                         role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M415.5 10.68L239.5 506.7C238.1 510.8 233.5 513 229.3 511.5C225.2 510.1 222.1 505.5 224.5 501.3L400.5 5.327C401.9 1.163 406.5-1.015 410.7 .4629C414.8 1.94 417 6.514 415.5 10.68V10.68zM493.5 122.2L629.5 250.2C631.1 251.7 632 253.8 632 256C632 258.2 631.1 260.3 629.5 261.8L493.5 389.8C490.3 392.9 485.2 392.7 482.2 389.5C479.1 386.3 479.3 381.2 482.5 378.2L612.3 256L482.5 133.8C479.3 130.8 479.1 125.7 482.2 122.5C485.2 119.3 490.3 119.1 493.5 122.2V122.2zM157.5 133.8L27.67 256L157.5 378.2C160.7 381.2 160.9 386.3 157.8 389.5C154.8 392.7 149.7 392.9 146.5 389.8L10.52 261.8C8.911 260.3 8 258.2 8 256C8 253.8 8.911 251.7 10.52 250.2L146.5 122.2C149.7 119.1 154.8 119.3 157.8 122.5C160.9 125.7 160.7 130.8 157.5 133.8V133.8z"></path>
+                    </svg>
+                    <h3 class="title mb-4">Flexibilidade</h3>
+                    <p style="max-width: 340px" class="relative z-10 text-gray-400/60">Headless ecommerce oferece
+                        flexibilidade e familiaridade para desenvolvedores front-end, onde estruturas comuns como o
+                        React podem ser usadas para obter excelentes resultados.</p></div>
+                <div class="">
+                    <svg class="fill-current h-12 w-12 mb-8 text-gray-400" viewBox="0 0 512 512" focusable="false"
+                         role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M510.4 195.2l-120-160c-1.5-2-3.9-3.2-6.4-3.2H128c-2.5 0-4.9 1.2-6.4 3.2l-120 160c-2.3 3.1-2.1 7.3 .5 10.2l248 272c1.5 1.7 3.7 2.6 5.9 2.6s4.4-1 5.9-2.6l248-272c2.6-2.8 2.8-7.1 .5-10.2zM383.8 53.1L488 192H272.6L383.8 53.1zM256 187.2L144.7 48H367.3L256 187.2zM128.2 53.1L239.4 192H24L128.2 53.1zM256 460.1L26.1 208H485.9L256 460.1z"></path>
+                    </svg>
+                    <h3 class="title mb-4">Propriedade completa sobre a arquitetura do site</h3>
+                    <p style="max-width: 340px" class="relative z-10 text-gray-400/60">Headless ecommerce oferece
+                        propriedade completa sobre a arquitetura do site com front-end e
+                        back-end desacoplados.</p></div>
+                <div class="">
+                    <svg class="fill-current h-12 w-12 mb-8 text-gray-400" viewBox="0 0 384 512" focusable="false"
+                         role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M80 512h224c26.5 0 48-21.5 48-48v-416C352 21.5 330.5 0 304 0h-224C53.5 0 32 21.5 32 48v416C32 490.5 53.5 512 80 512zM48 48c0-17.64 14.36-32 32-32h224c17.64 0 32 14.36 32 32v416c0 17.64-14.36 32-32 32h-224c-17.64 0-32-14.36-32-32V48zM144 456h96c4.406 0 8-3.578 8-8s-3.594-8-8-8h-96c-4.406 0-8 3.578-8 8S139.6 456 144 456z"></path>
+                    </svg>
+                    <h3 class="title mb-4">Eficácia do marketing</h3>
+                    <p style="max-width: 340px" class="relative z-10 text-gray-400/60">Eficácia do marketing para
+                        inovação sem prejudicar os processos de back-end.</p></div>
+                <div class="">
+                    <svg class="fill-current h-12 w-12 mb-8 text-gray-400" viewBox="0 0 640 512" focusable="false"
+                         role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M64 8c0-4.4 3.6-8 8-8h37.7c19 0 35.4 13.4 39.2 32H591.7c25.8 0 44.8 24 39 49.1L592.4 244.7c-5.9 25.3-28.5 43.3-54.5 43.3h-339l9.3 47.7c3.7 18.8 20.1 32.3 39.3 32.3H568c4.4 0 8 3.6 8 8s-3.6 8-8 8H247.5c-26.8 0-49.8-19-55-45.2L133.2 35.4C131 24.1 121.1 16 109.7 16H72c-4.4 0-8-3.6-8-8zM195.8 272H537.9c18.6 0 34.7-12.8 39-30.9L615 77.5c3.5-15.1-7.9-29.5-23.4-29.5H152l43.8 224zM240 496a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm0-80a48 48 0 1 1 0 96 48 48 0 1 1 0-96zm320 48a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm-80 0a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zM8 128H120c4.4 0 8 3.6 8 8s-3.6 8-8 8H8c-4.4 0-8-3.6-8-8s3.6-8 8-8zm0 64H136c4.4 0 8 3.6 8 8s-3.6 8-8 8H8c-4.4 0-8-3.6-8-8s3.6-8 8-8zm0 64H152c4.4 0 8 3.6 8 8s-3.6 8-8 8H8c-4.4 0-8-3.6-8-8s3.6-8 8-8z"></path>
+                    </svg>
+                    <h3 class="title mb-4">Velocidade para o mercado</h3>
+                    <p style="max-width: 340px" class="relative z-10 text-gray-400/60">Uma solução de arquitetura
+                        headless não é apenas escalável, mas também flexível e facilmente replicável, especialmente para
+                        o
+                        mercado internacional e omnicanal.</p></div>
+                <div class="">
+                    <svg class="fill-current h-12 w-12 mb-8 text-gray-400" viewBox="0 0 640 512" focusable="false"
+                         role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M562.3 2.351C565.5-.7732 570.5-.7732 573.7 2.351L637.7 66.35C639.2 67.85 640 69.88 640 72C640 74.12 639.2 76.16 637.7 77.66L573.7 141.7C570.5 144.8 565.5 144.8 562.3 141.7C559.2 138.5 559.2 133.5 562.3 130.3L612.7 80.01L407.1 80L407.9 80H79.1C62.33 80 47.1 94.33 47.1 112V332.1L31.1 348.1V112C31.1 85.49 53.49 64 79.1 64L612.7 64.01L562.3 13.67C559.2 10.54 559.2 5.476 562.3 2.352L562.3 2.351zM77.66 498.3C80.78 501.5 80.78 506.5 77.66 509.7C74.53 512.8 69.47 512.8 66.34 509.7L2.343 445.7C.8426 444.2 0 442.1 0 439.1C0 437.9 .8429 435.8 2.343 434.3L66.34 370.3C69.47 367.2 74.53 367.2 77.66 370.3C80.78 373.5 80.78 378.5 77.66 381.7L27.31 432L231.1 431.1L232.2 432H560C577.7 432 592 417.7 592 400V179.9L608 163.9V400C608 426.5 586.5 448 560 448L27.32 448L77.66 498.3zM480 392C480 352.2 512.2 320 552 320H560C564.4 320 568 323.6 568 328C568 332.4 564.4 336 560 336H552C521.1 336 496 361.1 496 392V408C496 412.4 492.4 416 488 416C483.6 416 480 412.4 480 408V392zM151.1 96C156.4 96 159.1 99.58 159.1 104V120C159.1 159.8 127.8 192 87.1 192H79.1C75.58 192 71.1 188.4 71.1 184C71.1 179.6 75.58 176 79.1 176H87.1C118.9 176 143.1 150.9 143.1 120V104C143.1 99.58 147.6 96 151.1 96zM416 256C416 309 373 352 319.1 352C266.1 352 223.1 309 223.1 256C223.1 202.1 266.1 160 319.1 160C373 160 416 202.1 416 256zM319.1 176C275.8 176 239.1 211.8 239.1 256C239.1 300.2 275.8 336 319.1 336C364.2 336 400 300.2 400 256C400 211.8 364.2 176 319.1 176z"></path>
+                    </svg>
+                    <h3 class="title mb-4">Maiores taxas de conversão</h3>
+                    <p style="max-width: 340px" class="relative z-10 text-gray-400/60">Os primeiros cinco segundos do
+                        tempo de carregamento de uma página têm o maior impacto em sua taxa de conversão, e é aqui que o
+                        headless brilha: o desempenho.</p></div>
+            </div>
+        </div>
+    </section>
+</main>
