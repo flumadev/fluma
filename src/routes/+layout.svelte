9c5cab7c61c5837f27a1f4b2f4d461c66ed55257
Luis Amorim <luisfelipeamorim@hotmail.com>
2023-04-15 19:40:31 -0300

    adição de mais meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,27 @@
 </script>
 
 <svelte:head>
+    <!-- HTML Meta Tags -->
     <title>Fluma // Agência digital</title>
     <meta name="description" content="Agência digital especializada em e-commerce, desenvolvimento web e aplicativos.">
-    <meta property="og:title" content="Fluma // Agência digital"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:image" content="/static/og.png"/>
-    <meta property="og:url" content="https://www.fluma.dev"/>
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://fluma.dev/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Fluma // Agência digital">
+    <meta property="og:description"
+          content="Agência digital especializada em e-commerce, desenvolvimento web e aplicativos.">
+    <meta property="og:image" content="https://fluma.dev/static/og.png">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="fluma.dev">
+    <meta property="twitter:url" content="https://fluma.dev/">
+    <meta name="twitter:title" content="Fluma // Agência digital">
+    <meta name="twitter:description"
+          content="Agência digital especializada em e-commerce, desenvolvimento web e aplicativos.">
+    <meta name="twitter:image" content="https://fluma.dev/static/og.png">
+
 </svelte:head>
 
 
